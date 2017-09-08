04dab2410f9c107384142dc2af22347b723824d5
Sidharth Ramesh <tornadoalert@gmail.com>
2017-09-08 20:02:24 +0530

    updated static file dir

--- noq/settings.py
+++ noq/settings.py
@@ -121,7 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 CELERY_BROKER_URL = config('REDISCLOUD_URL','django://')
 

c15b36e4764e686d9b4701b578e39fd65aecc75f
Sidharth Ramesh <tornadoalert@gmail.com>
2017-09-11 17:54:47 +0530

    celery beat working on windows

--- core/tasks.py
+++ core/tasks.py
@@ -1,12 +1,11 @@
 from celery import shared_task, task
+import requests, datetime
 
 @task
 def add(a,b):
     return a+b
 
 @task
-def send_request():
-    import requests, time
-    r = requests.post('https://requestb.in/1dvp3ch1', data={"ts":time.time()})
-    print r.status_code
-    print r.content
+def ping():
+    r = requests.post("https://requestb.in/1dvp3ch1",{"datetime":datetime.datetime.now()})
+    return r.status_code

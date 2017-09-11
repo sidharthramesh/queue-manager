c6721821fab951fe061610d77ed5e723531cf114
Sidharth Ramesh <tornadoalert@gmail.com>
2017-09-11 17:13:09 +0530

    added task to send request

--- core/tasks.py
+++ core/tasks.py
@@ -4,4 +4,9 @@ from celery import shared_task, task
 def add(a,b):
     return a+b
 
-
+@task
+def send_request():
+    import requests, time
+    r = requests.post('https://requestb.in/1dvp3ch1', data={"ts":time.time()})
+    print r.status_code
+    print r.content
